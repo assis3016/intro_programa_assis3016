9fc3f292c30c1d1c8e46d82e19707de9a287b41e
assis3016 <202405118091@alunos.estacio.br>
2025-03-07 01:01:59 -0300

    primeiros codes intro_programa

---
+++ .vscode/comparacao_dois_valores.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+int main()
+{
+    int numero1 = 8;
+    int numero2 = 5;
+    if (numero1 > numero2)
+    {
+        printf("o primeiro numero e maior que numero2/n");
+    };
+    return 0;
+}
