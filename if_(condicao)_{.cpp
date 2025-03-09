49aada410313213dc4deb68e1a19e4d95b01d812
assis3016 <202405118091@alunos.estacio.br>
2025-03-09 09:44:57 -0300

    exerc_ if_else

---
+++ if_(condicao)_{.cpp
@@ -0,0 +1,4 @@
+if (condicao)
+{
+    // bloco de codigo a ser executado
+}
---
+++ temperatura.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    float temperatura = 32.0;
+    if (temperatura > 30.0) {
+        printf("esta calor\n");
+    }
+    return 0;
+}
